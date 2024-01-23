d3da30a8f600406105809c669ab0551d50b5bd7f
lxq <18727097236@163.com>
2024-01-23 17:27:03 +0800

    second

--- admin/src/main/java/org/lxq/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/org/lxq/shortlink/admin/controller/UserController.java
@@ -1,6 +1,8 @@
 package org.lxq.shortlink.admin.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.lxq.shortlink.admin.common.convention.result.Result;
+import org.lxq.shortlink.admin.common.enums.UserErrorCodeEnum;
 import org.lxq.shortlink.admin.dto.resp.UserRespDTO;
 import org.lxq.shortlink.admin.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +18,14 @@ public class UserController {
 
 
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public UserRespDTO findByUsername(@PathVariable String username){
-        return userService.getUserByUsername(username);
+    public Result findByUsername(@PathVariable String username){
+        UserRespDTO result = userService.getUserByUsername(username);
+        if(result == null){
+            return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.USER_NULL.code()).
+                    setMessage(UserErrorCodeEnum.USER_NULL.message());
+        }else{
+            return new Result<UserRespDTO>().setCode("0").setData(result);
+        }
 
 
     }
--- admin/src/main/java/org/lxq/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/org/lxq/shortlink/admin/service/impl/UserServiceImpl.java
@@ -16,8 +16,12 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDo> implements 
     public UserRespDTO getUserByUsername(String username) {
         LambdaQueryWrapper<UserDo> eq = Wrappers.lambdaQuery(UserDo.class).eq(UserDo::getUsername, username);
         UserDo userDo = baseMapper.selectOne(eq);
-        UserRespDTO res = new UserRespDTO();
-        BeanUtils.copyProperties(userDo,res);
-        return res;
+        if(userDo != null){
+            UserRespDTO res = new UserRespDTO();
+            BeanUtils.copyProperties(userDo,res);
+            return res;
+        }
+
+        return null;
     }
 }
